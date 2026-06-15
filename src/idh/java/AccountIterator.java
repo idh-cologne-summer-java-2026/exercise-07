e463931b1e07d0a1d13397dceb1fe4fb24a24ead
leahk <leahkeppel@icloud.com>
2026-06-15 13:10:21 +0200

    first commit

---
+++ src/idh/java/AccountIterator.java
@@ -0,0 +1,19 @@
+package idh.java;
+
+import java.util.Iterator;
+
+public class AccountIterator implements Iterator<Account> {
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Account next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
