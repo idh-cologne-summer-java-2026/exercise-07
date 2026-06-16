533b0818625b78140f92a4011c605979530c74ee
ToonLinkHD <carlo.ital@web.de>
2026-06-17 00:39:52 +0200

    Update ATM.java

--- src/idh/java/ATM.java
+++ src/idh/java/ATM.java
@@ -2,30 +2,26 @@ package idh.java;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Random;
 
 public class ATM {
 
 	// initial cash in the ATM
 	int cash = 100;
 
-	// accounts known to the ATM
-	Account[] accounts = new Account[5];
+	private final Bank bank;
 
-	public ATM() {
-		// create accounts with varying balances
-		Random random = new Random();
-		for (int i = 0; i < accounts.length; i++) {
-			accounts[i] = new Account(i, random.nextInt(1000));
-		}
+	public ATM(Bank bank) {
+		this.bank = bank;
 	}
 
 	/**
-	 * Main command loop of the ATM Asks the user to enter a number, and passes this
-	 * number to the function cashout(...) which actually does the calculation and
-	 * produces money. If the user enters anything else than an integer number, the
-	 * loop breaks and the program exists
-	 */
+
+	* Main command loop of the ATM Asks the user to enter a number, and passes this
+	* number to the function cashout(...) which actually does the calculation and
+	* produces money. If the user enters anything else than an integer number, the
+	* loop breaks and the program exists
+
+	*/
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
@@ -36,7 +32,7 @@ public class ATM {
 				int amount = Integer.parseInt(br.readLine());
 				cashout(accountNumber, amount);
 			} catch (Exception e) {
-				e.printStackTrace();
+				System.out.println("Exiting ATM application.");
 				break;
 			}
 		}
@@ -66,36 +62,24 @@ public class ATM {
 		account.withdraw(amount);
 		cash += amount;
 		System.out.println("Ok, here is your money, enjoy!");
-
-	};
+	}
+	
+	protected Account getAccount(int id) {
+		for (Account account : bank) {
+			if (account.getId() == id) {
+				return account;
+			}
+		}
+		return null;
+	}
 
 	/**
 	 * Launches the ATM
 	 */
 	public static void main(String[] args) {
-		ATM atm = new ATM();
+		Bank meineBank = new Bank();
+		ATM atm = new ATM(meineBank);
+		
 		atm.run();
-		
-	AccountIterator iter = new AccountIterator (atm);
-		while (iter.hasNext()) {
-			Account account = iter.next();
-			System.out.println(account.getBalance());
-		}
-		
-	};
-
-	/**
-	 * Retrieves the account given an id.
-	 * 
-	 * @param id
-	 * @return
-	 */
-	protected Account getAccount(int id) {
-		for (int i = 0; i < accounts.length; i++) {
-			if (accounts[i].getId() == id)
-				return accounts[i];
-		}
-		return null;
-	}
-
+	}		
 }
